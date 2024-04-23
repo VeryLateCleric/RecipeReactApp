1e90690e6b71ec078a135cfdc22daf67230b175e
Kenneth <kennethyoung.2960@gmail.com>
2024-04-23 17:51:05 -0500

    Added formData, handleChange and handleSubmit to RecipeCreate. Added handleRecipeCreate to App.js

--- Project_Recipe_App_React_18_1/src/RecipeCreate.js
+++ Project_Recipe_App_React_18_1/src/RecipeCreate.js
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
 
 function RecipeCreate() {
+  const [formData, setFormData] = useState({
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  });
 
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
-  // TODO: Add the required submit and change handlers.
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onRecipeCreate(formData);
+    setFormData({
+      name: "" ,
+      cuisine: "",
+      photo: "",
+      ingredients: "",
+      preparation: "",
+    })
+  }
   
   return (
     <form name="create">
@@ -22,4 +42,8 @@ function RecipeCreate() {
   );
 }
 
-export default RecipeCreate;
+  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
+  // TODO: Add the required input and textarea form elements.
+  // TODO: Add the required submit and change handlers.
+
+  export default RecipeCreate;
