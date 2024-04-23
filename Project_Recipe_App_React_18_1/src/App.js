36522a80a57fb5827eca5c690ce4d39a15ab7473
Kenneth <kennethyoung.2960@gmail.com>
2024-04-23 17:56:49 -0500

    Implemented RecipeCreate module

--- Project_Recipe_App_React_18_1/src/App.js
+++ Project_Recipe_App_React_18_1/src/App.js
@@ -6,16 +6,18 @@ import RecipeData from "./RecipeData"
 
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
-
+  const handleRecipeCreate = (newRecipe) => {
+    setRecipes([...recipes, newRecipe]);
+  }
   // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
-  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+  // TODO:DONE Add the ability for the <RecipeCreate /> component to create new recipes.
 
   
   return (
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList />
-      <RecipeCreate />
+      <RecipeList recipes={recipes} />
+      <RecipeCreate onRecipeCreate={handleRecipeCreate}/>
     </div>
   );
 }
--- Project_Recipe_App_React_18_1/src/RecipeCreate.js
+++ Project_Recipe_App_React_18_1/src/RecipeCreate.js
@@ -12,24 +12,72 @@ function RecipeCreate() {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     onRecipeCreate(formData);
     setFormData({
-      name: "" ,
+      name: "",
       cuisine: "",
       photo: "",
       ingredients: "",
       preparation: "",
-    })
-  }
-  
+    });
+  };
+
   return (
-    <form name="create">
+    <form name="create" onSubmit={handleSubmit}>
       <table>
         <tbody>
+          <tr>
+            <td>Name:</td>
+            <td>
+              <input type="text" name="name" value={formData.name} />
+            </td>
+          </tr>
+          <tr>
+            <td>Cuisine:</td>
+            <td>
+              <input
+                type="text"
+                name="cuisine"
+                value={formData.cuisine}
+                onChange={handleChange}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Photo URL:</td>
+            <td>
+              <input
+                type="text"
+                name="photo"
+                value={formData.photo}
+                onChange={handleChange}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Ingredients:</td>
+            <td>
+              <textarea
+                name="ingredients"
+                value={formData.ingredients}
+                onChange={handleChange}
+              ></textarea>
+            </td>
+          </tr>
+          <tr>
+            <td>Preparation:</td>
+            <td>
+              <textarea
+                name="preparation"
+                value={formData.preparation}
+                onChange={handleChange}
+              ></textarea>
+            </td>
+          </tr>
           <tr>
             <td></td>
             <td>
@@ -42,8 +90,8 @@ function RecipeCreate() {
   );
 }
 
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
-  // TODO: Add the required submit and change handlers.
+// TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
+// TODO:DONE Add the required input and textarea form elements.
+// TODO:DONE Add the required submit and change handlers.
 
-  export default RecipeCreate;
+export default RecipeCreate;
