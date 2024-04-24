d3f489381726bed9fd7638dcb487a1808de46dc9
Kenneth <kennethyoung.2960@gmail.com>
2024-04-23 23:40:01 -0500

    Added handleDeleteRecipe to App component. Added formatted list to RecipeList for recipes. Create not responding and needs rework

--- Project_Recipe_App_React_18_1/src/App.js
+++ Project_Recipe_App_React_18_1/src/App.js
@@ -6,17 +6,24 @@ import RecipeData from "./RecipeData"
 
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
+
   const handleRecipeCreate = (newRecipe) => {
     setRecipes([...recipes, newRecipe]);
   }
-  // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
-  // TODO:DONE Add the ability for the <RecipeCreate /> component to create new recipes.
+
+  const handleDeleteRecipe = (index) => {
+    const updatedRecipes = recipes.filter((_, i) => i !== index);
+    setRecipes(updatedRecipes);
+  }
+
+  // TODO:DONE Add the ability for the <RecipeList /> component to list and delete an existing recipe.
+  // TODO:UNDONE Add the ability for the <RecipeCreate /> component to create new recipes.
 
   
   return (
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList recipes={recipes} />
+      <RecipeList recipes={recipes} onDeleteRecipe={handleDeleteRecipe}/>
       <RecipeCreate onRecipeCreate={handleRecipeCreate}/>
     </div>
   );
